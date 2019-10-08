3bc86ca9063c179c3659a91ffb674e0ac3fe94ae
野口和宏 <nowalicht313kn@gmail.com>
2019-10-08 22:41:10 +0900

    First commit

---
+++ YomikataApp/OutputViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OutputViewController.swift
+//  YomikataApp
+//
+//  Created by 野口和宏 on 2019/10/08.
+//  Copyright © 2019 野口和宏. All rights reserved.
+//
+
+import UIKit
+
+class OutputViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
